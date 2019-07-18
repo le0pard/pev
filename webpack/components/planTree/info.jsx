5fb96d71d70e8a4aed9b2a3e674f3d722d631721
Alexey Vasiliev <leopard.not.a@gmail.com>
2019-07-18 14:22:56 +0300

    round

--- webpack/components/planTree/info.jsx
+++ webpack/components/planTree/info.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import _round from 'lodash/round'
 import {
   MAXIMUM_DURATION_PROP,
   MAXIMUM_ROWS_PROP,
@@ -19,23 +20,23 @@ export default class PlanTreeInfo extends React.Component {
     return (
       <div className="plan-stats">
         <div>
-          <span className="stat-value">{plan[EXECUTION_TIME_PROP]}</span>
+          <span className="stat-value">{_round(plan[EXECUTION_TIME_PROP], 2)}</span>
           <span className="stat-label">execution time (ms)</span>
         </div>
         <div>
-          <span className="stat-value">{plan[PLANNING_TIME_PROP]}</span>
+          <span className="stat-value">{_round(plan[PLANNING_TIME_PROP], 2)}</span>
           <span className="stat-label">planning time (ms)</span>
         </div>
         <div>
-          <span className="stat-value">{plan[MAXIMUM_DURATION_PROP]}</span>
+          <span className="stat-value">{_round(plan[MAXIMUM_DURATION_PROP], 2)}</span>
           <span className="stat-label">slowest node (ms)</span>
         </div>
         <div>
-          <span className="stat-value">{plan[MAXIMUM_ROWS_PROP]}</span>
+          <span className="stat-value">{_round(plan[MAXIMUM_ROWS_PROP], 2)}</span>
           <span className="stat-label">largest node (rows)</span>
         </div>
         <div>
-          <span className="stat-value">{plan[MAXIMUM_COSTS_PROP]}</span>
+          <span className="stat-value">{_round(plan[MAXIMUM_COSTS_PROP], 2)}</span>
           <span className="stat-label">costliest node</span>
         </div>
       </div>
